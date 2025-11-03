11c65a273657b1edef67f2a65e9aae41af7de0bd
DeepGhetia <17cs030.deep@sjec.ac.in>
2025-11-03 18:49:58 +0530

    new chnages

--- day_infinite.py
+++
@@ -1,4 +0,0 @@
-with open('C:/Users/hp/Desktop/python/read.txt','r') as file:
-    header = file.readline().strip().split(',')
-    footer = [i.split(',') for i in file.read().splitlines()]
-    print(header,footer)
